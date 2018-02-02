538b9f783fa3da91f25c61939a1ac3efa74fc9a2
pbarera1 <phil.barera@gmail.com>
2018-02-02 15:04:40 -0800

    cleanup

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const request = require('request');
-// const puppeteer = require('puppeteer');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,42 +24,6 @@ app.get('/advisory', (req, res) => {
 	// is the water safe?
 
 });
-
-//puppeteer
-// async function run() {
-// 	const browser = await puppeteer.launch();
-// 	const page = await browser.newPage();
-
-// 	await page.goto('http://www.sdbeachinfo.com/');
-// 	await page.waitFor(2*1000);
-
-// 	const text = page.evaluate(() => document.querySelector('.footer-bottom-link').textContent);
-// 	console.log(text);
-// }
-// run();
-
-
-//cheerio
-// app.get('/advisory', (req, res) => {
-// 	// is the water safe?
-// 	request('http://www.sdcoastkeeper.org/learn/swimmable/san-diego-water-quality/beach-advisories', function (error, response, body) {
-// 			console.log('error:', error); // Print the error if one occurred and handle it
-// 			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-// 			const $ = cheerio.load(body);
-// 			const advisories = $('.entry');
-// 			console.log(advisories);
-// 			const isAdvisory = {};
-// 			advisories.each(function(index, elem) {
-// 				let beachName = elem.text().split('<br/>')[0].toLowerCase();
-// 				if(beachName.includes('ocean beach') || beachName.includes('la jolla') || beachName.includes('mission beach')) {
-// 					alert = $('#advisory-view ul li p a:last-child').text();
-// 					isAdvisory[beachName] = alert;
-// 				}
-// 			});
-// 			res.send(isAdvisory)
-// 	});
-// });
 
 // api route
 app.get('/api/hello', (req, res) => {
--- src/helpers/getDayOfWeek.js
+++ src/helpers/getDayOfWeek.js
@@ -1,44 +1,21 @@
 const getDayOfWeek = (daysInFuture) => {
-	const today = new Date().getDay();
-	let day = today + daysInFuture;
+	if (!daysInFuture) return 'Today';
 
-	switch (day) {
-		case 1:
-		day = "Monday";
-		break;
-		case 2:
-		day = "Tuesday";
-		break;
-		case 3:
-		day = "Wednesday";
-		break;
-		case 4:
-		day = "Thursday";
-		break;
-		case 5:
-		day = "Friday";
-		break;
-		case 6:
-		day = "Saturday";
-		break;
-		case 7:
-		day = "Sunday";
-		break;
-		case 8:
-		day = "Monday";
-		break;
-		case 9:
-		day = "Tuesday";
-		break;
-		case 10:
-		day = "Wenesday";
-		break;
-	}
-	if (!daysInFuture) {
-		return 'Today';
-	} else {
-		return day;
-	}
+	const today = new Date().getDay();
+	let day = today + daysInFuture -1;
+
+	const daysOfWeek = [
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday',
+		'Sunday'
+	];
+
+	const remainder = day % daysOfWeek.length;
+	return daysOfWeek[remainder];
 }
 
 export default getDayOfWeek;
